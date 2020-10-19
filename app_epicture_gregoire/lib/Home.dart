e19b1f7c43175334c87691c426c729db718a9c5d
Gregoire Brasseur <gregoirebrasseurpro@gmail.com>
2020-10-19 18:27:29 +0200

    fix: clean code home_page

--- app_epicture_gregoire/lib/Home.dart
+++ app_epicture_gregoire/lib/Home.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:developer';
+import 'package:app_epicture_gregoire/Acount.dart';
 import 'Global.dart';
 import 'HomeCard.dart';
 import 'package:flutter/material.dart';
@@ -7,43 +9,95 @@ import 'package:http/http.dart' as http;
 import 'ImgurGaleryObject.dart';
 import 'dart:io';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   BasicCall user;
-  List<ImgurGallery> gallery;
   Home({this.user});
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<ImgurGallery> gallery = new List<ImgurGallery>();
+  bool hasdata = false;
+
+  FutureBuilder<List<ImgurGallery>> hello() {
+    return FutureBuilder<List<ImgurGallery>>(
+      future: widget.user.getGallery(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          gallery.addAll(snapshot.data);
+          return ListView(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  for (var item in gallery)
+                    if (item.images.first.mp4_size == null)
+                      HomeCard(
+                        urlpicture: item.images.first.link,
+                        customHeigh: item.images.first.height,
+                      ),
+                ],
+              )
+            ],
+          );
+        } else if (snapshot.hasError) {
+          print('error');
+          return Text("${snapshot.hasError}");
+        }
+        print("circular");
+        return CircularProgressIndicator();
+      },
+    );
+  }
+
+  void updateGallery(List<ImgurGallery> filler) {}
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: FutureBuilder<List<ImgurGallery>>(
-        future: user.getGallery(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            gallery = snapshot.data;
-            return ListView(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    for (var item in snapshot.data)
-                      if (item.images.first.mp4_size == null)
-                        HomeCard(
-                          urlpicture: item.images.first.link,
-                          customHeigh: item.images.first.height,
-                        ),
-                  ],
-                )
-              ],
-            );
-          } else if (snapshot.hasError) {
-            print('error');
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        },
-      ),
+      child: hello(),
     );
   }
 }
+
+// class Home extends StatelessWidget {
+//   BasicCall user;
+//   List<ImgurGallery> gallery;
+//   Home({this.user});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//       child: FutureBuilder<List<ImgurGallery>>(
+//         future: user.getGallery(),
+//         builder: (context, snapshot) {
+//           if (snapshot.hasData) {
+//             gallery = snapshot.data;
+//             return ListView(
+//               children: [
+//                 Column(
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: <Widget>[
+//                     for (var item in snapshot.data)
+//                       if (item.images.first.mp4_size == null)
+//                         HomeCard(
+//                           urlpicture: item.images.first.link,
+//                           customHeigh: item.images.first.height,
+//                         ),
+//                   ],
+//                 )
+//               ],
+//             );
+//           } else if (snapshot.hasError) {
+//             print('error');
+//             return Text("${snapshot.error}");
+//           }
+//           return CircularProgressIndicator();
+//         },
+//       ),
+//     );
+//   }
+// }
+
 // class Home extends StatefulWidget {
 //   List<ImgurGallery> homeGalery;
 //   @override
